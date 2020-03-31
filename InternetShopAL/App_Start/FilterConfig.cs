652d7f5011272c770d0190496e6e38711319372b
Max <kolotseymax@gmail.com>
2020-03-31 21:20:25 +0300

    Add project files.

---
+++ InternetShopAL/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace InternetShopAL
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
